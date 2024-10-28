b9ed91cea255e2b8155cb031374c68a7b289f8d0
divPatel <divpatel4081@gmail.com>
2024-10-28 19:36:07 +0600

    app.listen implemented

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
 const express = require("express");
+const mainRouter = require("./routes/index");
+const app = express();
+const cors = require("cors");
+
+app.use("/api/v1",mainRouter);
+app.use(cors());
+app.use(express.json());
 
 
+app.listen(3000,() => {
+    console.log("server is listening on port no 3000");
+})
