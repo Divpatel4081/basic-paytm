b06cff52c38bae6df51c38b722bf005bd77e1040
divPatel <divpatel4081@gmail.com>
2024-10-28 21:05:46 +0600

    bulk query-route implemented

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -108,4 +108,33 @@ router.put('/', authMiddleware , async (req,res) => {
     })
 })
 
+
+//bulk queary
+router.get("/bulk" , async (req,res) => {
+    const filter = req.query.filter;
+    const users = await User.find({
+        $or:[
+            {
+                firstName: {
+                    $regex: filter
+                }
+            },
+            {
+                lastName:{
+                    $regex:filter
+                }
+            }
+        ]
+    })
+    res.json({
+        user:users.map((user) => {
+            return {
+                username:user.username,
+                firstName: user.firstName,
+                lastName: user.lastName,
+            }
+        })
+    })
+})
+
 module.exports = router;
